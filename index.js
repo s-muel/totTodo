654a89f87e9a479644a6f2d7668b48441c1d7f10
s-muel <samuel.essuman@amalitech.org>
2021-10-14 16:16:09 +0000

    fixed proxy error... installed missing dependency

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // const express = require("express");
 import express from 'express';
 import  mongoose  from "mongoose";
-// import TodoModel from './schemas/todo_schema.js';
+import TodoModel from './schemas/todo_schema.js';
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -13,7 +13,7 @@ const db = process.env.CON_STRING;
 
 
 //Mongoose.connect('mongodb+srv://sammy0288:sammy0288@cluster0.68gyj.mongodb.net/todo_db?retryWrites=true&w=majority',{
-mongoose.connect("mongodb+srv://sammy0288:sammy0288@cluster0.68gyj.mongodb.net/todo_db?retryWrites=true&w=majority",{
+mongoose.connect(db,{
 useNewUrlParser:true,
     useUnifiedTopology:true,
 }).then(()=>{
@@ -36,104 +36,104 @@ app.get("/", (req, res) => {
     })
 })
 
-// // getting all todo
-// app.get("/todo", async(req, res) => {
-//     const todoModel = await TodoModel.find({});
-//     if(todoModel){
-//         return res.status(200).json({
-//             status:true,
-//             message: 'Todos fetched successfully',
-//             data: todoModel
-//         })
-//     }else{
-//         return res.status(400).json({
-//             status:false,
-//             message: 'Todos not found'
-//         })
-//     }
-//     })
-// // get specific todo by id
-// app.get("/todo/:id",async (req, res) => {
-//     const {id} = req.params;
+// getting all todo
+app.get("/todo", async(req, res) => {
+    const todoModel = await TodoModel.find({});
+    if(todoModel){
+        return res.status(200).json({
+            status:true,
+            message: 'Todos fetched successfully',
+            data: todoModel
+        })
+    }else{
+        return res.status(400).json({
+            status:false,
+            message: 'Todos not found'
+        })
+    }
+    })
+// get specific todo by id
+app.get("/todo/:id",async (req, res) => {
+    const {id} = req.params;
 
-//     const todoModel = await TodoModel.findById(id);
-//     if(todoModel){
-//         return res.status(200).json({
-//             status:true,
-//             message: 'Todos fetched successfully',
-//             data: todoModel
-//         })
-//     }else{
-//         return res.status(400).json({
-//             status:false,
-//             message: 'Todos not found'
-//         })
-//     }
-// })
-// // creating todo
-// app.post("/todo", async(req, res) => {
-//     const {title,description,date_time, status} = req.body;
+    const todoModel = await TodoModel.findById(id);
+    if(todoModel){
+        return res.status(200).json({
+            status:true,
+            message: 'Todos fetched successfully',
+            data: todoModel
+        })
+    }else{
+        return res.status(400).json({
+            status:false,
+            message: 'Todos not found'
+        })
+    }
+})
+// creating todo
+app.post("/todo", async(req, res) => {
+    const {title,description,date_time, status} = req.body;
 
-//     const todoModel = await TodoModel.create({
-//         title,
-//         description,
-//         date_time,
-//         status
-//     })
-//         if(todoModel){
-//             return res.status(201).json({
-//                 status:true,
-//                 message: 'Todos created',
-//                 data: todoModel
-//             })
-//         }else{
-//             return res.status(400).json({
-//                 status:false,
-//                 message: 'Todos failed to create'
-//             })
-//         }
-//     })
-// //updating
-// app.patch("/todo/:id", async(req, res) => {
+    const todoModel = await TodoModel.create({
+        title,
+        description,
+        date_time,
+        status
+    })
+        if(todoModel){
+            return res.status(201).json({
+                status:true,
+                message: 'Todos created',
+                data: todoModel
+            })
+        }else{
+            return res.status(400).json({
+                status:false,
+                message: 'Todos failed to create'
+            })
+        }
+    })
+//updating
+app.patch("/todo/:id", async(req, res) => {
 
-// try {
-//     const {id} = req.params;
-//     const {status} = req.body;
+try {
+    const {id} = req.params;
+    const {status} = req.body;
 
-//     const todoModel = await TodoModel.updateOne({status: status}).where({_id: id});
-//     if(todoModel){
-//         return res.status(200).json({
-//             status:true,
-//             message: 'Todos marked as completed!',
-//             data: todoModel
-//         })
-//     }else{
-//         return res.status(400).json({
-//             status:false,
-//             message: 'Todos failed to update'
-//         })
-//     }
+    const todoModel = await TodoModel.updateOne({status: status}).where({_id: id});
+    if(todoModel){
+        return res.status(200).json({
+            status:true,
+            message: 'Todos marked as completed!',
+            data: todoModel
+        })
+    }else{
+        return res.status(400).json({
+            status:false,
+            message: 'Todos failed to update'
+        })
+    }
     
-// } catch (error) {
-//     console.log(error);
-// }
-// })
-// // to delete 
-// app.delete('/todo/:id', async(req,res)=>{
-//     const todoModel = await TodoModel.findByIdAndDelete(req.params.id);
-//     if(todoModel){
-//         return res.status(200).json({
-//             status:true,
-//             message: 'Todo deleted!',
-//             data: todoModel
-//         })
-//     }else{
-//         return res.status(400).json({
-//             status:false,
-//             message: 'Todos failed to delete'
-//         })
-//     }
-//     })
+} catch (error) {
+    console.log(error);
+}
+})
+// to delete 
+app.delete('/todo/:id', async(req,res)=>{
+    const todoModel = await TodoModel.findByIdAndDelete(req.params.id);
+    if(todoModel){
+        return res.status(200).json({
+            status:true,
+            message: 'Todo deleted!',
+            data: todoModel
+        })
+    }else{
+        return res.status(400).json({
+            status:false,
+            message: 'Todos failed to delete'
+        })
+    }
+    })
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
